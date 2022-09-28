016dfeca0204143bcdd0c03f66dcd9a09a46c270
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2022-09-28 11:38:13 -0700

    Three things are added. First the telegram model class is created. Second the provider and intl packages are added. Third the provider class is created.

---
+++ telegram_app_clone/lib/Model/telegram.dart
@@ -0,0 +1,13 @@
+class TelegramModel {
+  final String name;
+  final String imageName;
+  final String description;
+  final String date;
+
+  TelegramModel({
+    required this.name,
+    required this.imageName,
+    required this.description,
+    required this.date,
+  });
+}
---
+++ telegram_app_clone/lib/Model/telegramProvider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:telegram_app_clone/Model/telegram.dart';
+
+class TelegramProvider with ChangeNotifier {
+  List<TelegramModel> _userList = [
+    TelegramModel(
+      name: 'Tariku Birhan',
+      imageName: 'Assets/IMG_20210130_174525.jpg',
+      description: 'Hello baba!! How you doing?? I\'ll call you soon!',
+      date: DateFormat.yMMMd().format(DateTime.now()),
+    ),
+    TelegramModel(
+      name: 'Kalkidan Nigussie',
+      imageName: 'Assets/freestocks-8a95EVm0ovQ-unsplash.jpg',
+      description:
+          'Are u okey Kalichu?? I\'ll come you soon and see you honey!',
+      date: DateFormat.yMMMd().format(DateTime.now()),
+    ),
+    TelegramModel(
+      name: 'Yoseph Alamrew',
+      imageName: 'Assets/milad-shams-FttBXHp7VZw-unsplash.jpg',
+      description:
+          'The project get secceded! Thank you for your support jossy!',
+      date: DateFormat.yMMMd().format(DateTime.now()),
+    ),
+  ];
+  List<TelegramModel> get userList {
+    return [..._userList];
+  }
+}
