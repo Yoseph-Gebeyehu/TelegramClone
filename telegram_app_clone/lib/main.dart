22048a4ac29047e839b1444b6ae59c7c7f21b1b7
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2022-09-28 11:57:28 -0700

    The main method is added at main.dart file

--- telegram_app_clone/lib/main.dart
+++ telegram_app_clone/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'Screen/telegram_main.dart';
 
+void main() {
+  runApp(const TelegramHome());
+}
+
 class TelegramHome extends StatelessWidget {
   const TelegramHome({Key? key}) : super(key: key);
 
